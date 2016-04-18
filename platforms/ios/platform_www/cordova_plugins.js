3e8479394d81a9cf7bfdbd96a64456fa026cda77
yitoman <mivillarroelm@gmail.com>
2016-04-18 18:11:06 -0400

    wewewwwww

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -1,14 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/com.alexwasner.plugins.orientationlock/www/OrientationLock.js",
-        "id": "com.alexwasner.plugins.orientationlock.OrientationLock",
-        "pluginId": "com.alexwasner.plugins.orientationlock",
-        "clobbers": [
-            "OrientationLock"
-        ]
-    },
-    {
         "file": "plugins/cordova-plugin-geolocation/www/Coordinates.js",
         "id": "cordova-plugin-geolocation.Coordinates",
         "pluginId": "cordova-plugin-geolocation",
@@ -52,7 +44,6 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.alexwasner.plugins.orientationlock": "0.0.1",
     "cordova-plugin-geolocation": "2.1.0",
     "cordova-plugin-splashscreen": "3.2.1",
     "cordova-plugin-whitelist": "1.2.0"
