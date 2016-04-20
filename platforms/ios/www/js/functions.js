eb5435c94bc78d3fb26630489f801d8c5f5f1938
yitoman <mivillarroelm@gmail.com>
2016-04-20 16:17:24 -0400

    Test spinner2

--- platforms/ios/www/js/functions.js
+++ platforms/ios/www/js/functions.js
@@ -7,11 +7,21 @@ $(document).ready(function() {
 	$('a.e-slide').on('click', function () {
 		enlace = $(this).attr('data-enlace');
 		item   = $(this).attr('href');
+		
+		  spinnerplugin.show({
+		      overlay: false,    // defaults to true
+		      timeout: 0,       // defaults to 0 (no timeout)
+		      fullscreen: true,  // defaults to false
+		  });
+
+
 		$(item).load(enlace);
 		//console.log("direcciones: " + enlace+ " - " +item);
 
 		$('#wrapper').scrollTo($(this).attr('href'), 500);		
 		
+		 spinnerplugin.hide();
+		 
 		return false;
 	});
 	$('a.back').on('click', function () {
